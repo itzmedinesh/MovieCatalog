e1b1c02a8c415e05104786dc07d08adf53c15f79
Dinesh Subramanian <dineshsubramanian@Dineshs-MacBook-Pro-2.local>
2025-04-22 08:38:55 +0530

    init commit

---
+++ src/test/java/io/demo/MovieCatalog/MovieCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.demo.MovieCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
